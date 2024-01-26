0290c0daf6e9c12526bbea12d075b4697d5afd79
Hamza-Mujahid <66218657+Ahsan-Ah-med@users.noreply.github.com>
2024-01-26 18:18:34 +0500

    Add incomplete assignment 14

---
+++ Module A Batch 11/Assignment 14/index.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $('.banner').slick({
+        lazyLoad: 'lazy',
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+    });
+});
+
+
