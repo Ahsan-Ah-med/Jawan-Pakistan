acb991d06444f1eab882ca59ec641bf78833089b
Hamza-Mujahid <66218657+Ahsan-Ah-med@users.noreply.github.com>
2024-02-07 12:09:22 +0500

    Update Mini Heckaton

--- Module A Batch 11/Mini Hackaton/index.js
+++ Module A Batch 11/Mini Hackaton/index.js
@@ -8,6 +8,29 @@ $(document).ready(function () {
     dots: false,
     prevArrow:$('.preArrow'),
     nextArrow:$('.nextArrow'),
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        }
+      },
+      {
+        breakpoint: 700,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        }
+      }
+    ]
   });
   $(".testimonial_box").slick({
     slidesToShow: 1,
@@ -18,5 +41,19 @@ $(document).ready(function () {
     dots: false,
     prevArrow:$('.testimonial .preArrow'),
     nextArrow:$('.testimonial .nextArrow'),
+  });
+});
+addEventListener("DOMContentLoaded", (event) => {
+  closeBtn.addEventListener("click", () => {
+    mobile_menu.classList.remove("Active");
+    closeOverlay.classList.remove("Active");
+  });
+  closeOverlay.addEventListener("click", () => {
+    mobile_menu.classList.remove("Active");
+    closeOverlay.classList.remove("Active");
+  });
+  humbugger.addEventListener("click", () => {
+    mobile_menu.classList.add("Active");
+    closeOverlay.classList.add("Active");
   });
 });
