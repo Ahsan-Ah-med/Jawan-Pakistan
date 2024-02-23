7f4a3225f490937e4833b98cec205e188c2bb512
Ahsan Ahmed <Ahsan.Ahmed.026@gmail.com>
2024-02-23 21:16:47 +0500

    Add JavaScript Class Work 2

---
+++ Module A Batch 11/JavaScript/Class Work/class 2/app.js
@@ -0,0 +1,27 @@
+// var table = prompt();
+// var number = 1;
+
+// document.write(table + " x " + number + " = " + number++ * table + "<br>");
+// document.write(table + " x " + number + " = " + number++ * table + "<br>");
+// document.write(table + " x " + number + " = " + number++ * table + "<br>");
+// document.write(table + " x " + number + " = " + number++ * table + "<br>");
+// document.write(table + " x " + number + " = " + number++ * table + "<br>");
+// document.write(table + " x " + number + " = " + number++ * table + "<br>");
+// document.write(table + " x " + number + " = " + number++ * table + "<br>");
+// document.write(table + " x " + number + " = " + number++ * table + "<br>");
+// document.write(table + " x " + number + " = " + number++ * table + "<br>");
+// document.write(table + " x " + number + " = " + number++ * table + "<br>");
+
+var name = prompt("Enter Your Name");
+var myclass = prompt("Enter Your Class");
+var age = +prompt("Enter Your Age");
+var fnum = +prompt("Enter Your English Number");
+var snum = +prompt("Enter Your Math Number");
+var tnum = +prompt("Enter Your Computer Number");
+
+document.write("Your Name Is " + name + "<br>");
+document.write("Your Class Is " + myclass + "<br>");
+document.write("Your Age Is " + age + "<br>");
+document.write("Your English Subject Number Is " + fnum + "<br>");
+document.write("Your Math Subject Number Is " + snum + "<br>");
+document.write("Your Computer Subject Number Is " + tnum + "<br>");
