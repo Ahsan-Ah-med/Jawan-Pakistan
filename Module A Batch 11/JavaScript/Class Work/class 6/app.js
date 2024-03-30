c8198bedf7744258983d4441f84fe561b1531b55
Ahsan Ahmed <Ahsan.Ahmed.026@gmail.com>
2024-03-30 22:58:45 +0500

    Add assignment 19

---
+++ Module A Batch 11/JavaScript/Class Work/class 6/app.js
@@ -0,0 +1,37 @@
+// var inputValue = prompt("Enter Your Text Hear");
+// var revText = "";
+// var flag = true
+// for (var i = inputValue.length - 1; i >= 0; i--) {
+//     revText += inputValue[i];
+//     if(revText == inputValue){
+//         flag = true
+//     } else{
+//         flag = false
+//   }
+// }
+// if(flag){
+//     console.log("Your Condition is match")
+// } else{
+//     console.log("Your Condition is Not match")
+// }
+// for (var i = inputValue.length - 1; i >= 0; i--) {
+//     revText += inputValue[i];
+// }
+// if(revText == inputValue){
+//     console.log("Your Condition is match")
+// } else{
+//     console.log("Your Condition is Not match")
+// }
+// console.log(revText);
+
+// var randomValue =
+//   "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstvuxyz1234567890!@#$%";
+// var password = "";
+// for (var i = 0; i < 10; i++) {
+//   var char = Math.floor(Math.random() * randomValue.length);
+//   password += randomValue[char];
+// }
+// console.log(password);
+
+// var date = new Date();
+// date.getTime()
