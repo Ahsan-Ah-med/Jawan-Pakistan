6b3043a247c671bb3cd1b84ee814cb392da95926
Hamza-Mujahid <66218657+Ahsan-Ah-med@users.noreply.github.com>
2024-06-24 20:02:12 +0500

    Fix fetching error in API and add loader, complete App

--- Module B Batch 13/Github User Finder App/app.js
+++ Module B Batch 13/Github User Finder App/app.js
@@ -1,7 +1,38 @@
-const userSearch = "Ahsan-Ah-med";
-
 const userFetch = async (search) => {
   console.log(search)
+  document.querySelector(".userCards").innerHTML = `
+  <div class="loader">
+  <div class="loaderMiniContainer">
+    <div class="barContainer">
+      <span class="bar"></span>
+      <span class="bar bar2"></span>
+    </div>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 101 114"
+      class="svgIcon"
+    >
+      <circle
+        stroke-width="7"
+        stroke="black"
+        transform="rotate(36.0692 46.1726 46.1727)"
+        r="29.5497"
+        cy="46.1727"
+        cx="46.1726"
+      ></circle>
+      <line
+        stroke-width="7"
+        stroke="black"
+        y2="111.784"
+        x2="97.7088"
+        y1="67.7837"
+        x1="61.7089"
+      ></line>
+    </svg>
+  </div>
+</div>
+`
   await fetch(`https://api.github.com/users/${search}`)
     .then((res) => res.json())
     .then((data) => {
@@ -16,7 +47,6 @@ const userFetch = async (search) => {
         </div>
         <div class="bottom">
           <div class="content">
-            <span class="name">User Name: ${data?.login}</span>
             <span class="name">Name: ${data?.name}</span>
             <span class="about-me">${data?.bio}</span>
           </div>
@@ -39,30 +69,66 @@ document.querySelector('.form').addEventListener('submit', (e) => {
   userFetch(search)
 })
 
-// const userload = async () => {
-//   await fetch(`https://api.github.com/users`)
-//     .then((res) => res.json())
-//     .then((data) => {
-//       data.map(async (e) => {
-//         document.querySelector(".userCards").innerHTML += `<div class="card">
-//         <button class="mail">
-//             <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-mail"><rect width="20" height="16" x="2" y="4" rx="2"></rect><path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path></svg>
-//         </button>
-//         <div class="profile-pic">
-//             <img src="${e?.avatar_url}"/>
-//         </div>
-//         <div class="bottom">
-//             <div class="content">
-//                 <span class="name">${e?.login}</span>
-//                 <span class="about-me">Lorem ipsum dolor sit amet consectetur adipisicinFcls </span>
-//             </div>
-//             <a href="${e?.html_url}" target="_blank" class="button">Contact Me</a>
-//            </div>
-//         </div>
-//     </div>`;
-//       });
-//     })
-//     .catch((err) => console.log(err));
-// };
+const userload = async () => {
+  document.querySelector(".userCards").innerHTML = `
+  <div class="loader">
+  <div class="loaderMiniContainer">
+    <div class="barContainer">
+      <span class="bar"></span>
+      <span class="bar bar2"></span>
+    </div>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 101 114"
+      class="svgIcon"
+    >
+      <circle
+        stroke-width="7"
+        stroke="black"
+        transform="rotate(36.0692 46.1726 46.1727)"
+        r="29.5497"
+        cy="46.1727"
+        cx="46.1726"
+      ></circle>
+      <line
+        stroke-width="7"
+        stroke="black"
+        y2="111.784"
+        x2="97.7088"
+        y1="67.7837"
+        x1="61.7089"
+      ></line>
+    </svg>
+  </div>
+</div>
+`
+  await fetch(`https://api.github.com/users`)
+    .then((res) => res.json())
+    .then((data) => {
+      document.querySelector(".userCards").innerHTML = '';
+      data.map(async (e) => {
+        // console.log(e)
+        document.querySelector(".userCards").innerHTML += `<div class="card">
+        <button class="mail">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-mail"><rect width="20" height="16" x="2" y="4" rx="2"></rect><path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"></path></svg>
+        </button>
+        <div class="profile-pic">
+            <img src="${e?.avatar_url}"/>
+        </div>
+        <div class="bottom">
+          <div class="content">
+            <span class="name"></span>
+            <span class="name">User Name: ${e?.login}</span>
+          </div>
+          <div class="bottom-bottom">
+            <a href="${e?.html_url}" target="_blank" class="button">Contact Me</a>
+          </div>
+        </div>
+    </div>`;
+      });
+    })
+    .catch((err) => console.log(err));
+};
 
-// userload();
+userload();
